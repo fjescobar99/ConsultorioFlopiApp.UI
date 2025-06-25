f8384fc95bd86172065bfa1075b0f17d83b5e3b2
Francisco Jose Escobar <fjescobar99@gmail.com>
2025-06-24 23:51:47 -0300

    se agrego tabla de pacientes

--- src/app/patients/patient-page/patient-page.component.ts
+++ src/app/patients/patient-page/patient-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PatientListComponent } from "../patient-list/patient-list.component";
 
 @Component({
   selector: 'app-patient-page',
   standalone: true,
-  imports: [],
+  imports: [PatientListComponent],
   templateUrl: './patient-page.component.html',
   styleUrl: './patient-page.component.css'
 })
